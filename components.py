2a3c4a6b458ab4f50f24b397c4d37ee3190ce386
Zafeer Mahmood <zafeer7460@gmail.com>
2023-02-26 23:30:50 +0500

    ui changes

--- components.py
+++ components.py
@@ -2,11 +2,18 @@ import requests
 import pandas as pd
 import streamlit as st
 import altair as alt
-from textblob import TextBlob
+import re
+import unicodedata
+import nltk
+from nltk.corpus import stopwords
+from transformers import pipeline
+
+#hugging face model can be change to any trained model hosted on huggingface.co/models
+specific_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
 
 
-
-
+#funtion get crypto data from coingecko API and make a chart with altair and streamlit 
+# @parametrs: select - the crypto name, selectInterval - the number of days to show
 def chart(select, selectInterval):
     endpoint = f'https://api.coingecko.com/api/v3/coins/{select}/market_chart'
     params = {
@@ -17,44 +24,114 @@ def chart(select, selectInterval):
     response = requests.get(endpoint, params=params)
     data = response.json()
 
-    if(response.status_code != 200):
+    if (response.status_code != 200):
         st.write('Error while gettinf data for CoinGecko API :(')
     else:
         df = pd.DataFrame(data['prices'], columns=['timestamp', 'price'])
         max_price = df['price'].max()
         min_price = df['price'].min()
+        mean_price = df['price'].mean()
         df['datetime'] = pd.to_datetime(df['timestamp'], unit='ms')
 
         # st.line_chart(df,x='datetime',y='price')
         myScale = alt.Scale(domainMax=max_price, domainMin=min_price)
         lineChart = alt.Chart(df).mark_line(clip=True).encode(
-        x='datetime',
-        y=alt.Y('price',scale=myScale)
+            x='datetime',
+            y=alt.Y('price', scale=myScale)
         ).interactive()
         st.altair_chart(lineChart,use_container_width=True)
-            
-        
+        return [max_price, min_price, mean_price]
+
+# funtion thats makes a dataframe with the posts from reddit
+#@parameter responseReddit - the response from the reddit API and an emty dataFrame
+#@return df_post_reddit - the dataframe with the posts
 @st.cache(ttl=24*60*60)
-def displayDf(responseReddit, df_post_reddit ):
-    for post in responseReddit.json()['data']['children']: # iterate over the posts to make a df to show
+def displayDf(responseReddit):
+    df_post_reddit=pd.DataFrame()
+    # iterate over the posts to make a df to show
+    for post in responseReddit.json()['data']['children']:
         df_post_reddit = df_post_reddit.append(
-        {
-            'title': post['data']['title'],
-            'author_fullname': post['data']['author_fullname'],
-            'selftext': post['data']['selftext'],
-            'upvote_ratio': post['data']['upvote_ratio'],
-            'post_url': post['data']['url'],
-            'num_comments': post['data']['num_comments'],
-            'post_upvote': post['data']['ups'],
-            'link_flair_text': post['data']['link_flair_text'],
-        }, ignore_index=True)
-    return df_post_reddit # show the dataframe with streamlit      
+            {
+                'title': post['data']['title'],
+                'author_fullname': post['data']['author_fullname'],
+                'selftext': post['data']['selftext'],
+                'upvote_ratio': post['data']['upvote_ratio'],
+                'post_url': post['data']['url'],
+                'num_comments': post['data']['num_comments'],
+                'post_upvote': post['data']['ups'],
+                'link_flair_text': post['data']['link_flair_text'],
+                'Sentiment': specific_model(post['data']['title'])[0]['label'],
+                'Sentiment_score': specific_model(post['data']['title'])[0]['score'],
+            }, ignore_index=True)
+    return df_post_reddit  # show the dataframe with streamlit
 
 
-def rating(string):
-    text_blob_obj = TextBlob(string)
-    sentiment_score = text_blob_obj.sentiment.polarity
-    sentiment_score_0_to_100 = (sentiment_score + 1) * 50
-    return sentiment_score_0_to_100;
+
+#funtion that cleans the unnerrcessary data from the text eg. stopwords, links, etc.
+# @parametrs: text - the text to clean
+# @return words - the cleaned text
+def basic_clean(text):
+    stopwords = nltk.corpus.stopwords.words('english')
+    text = (unicodedata.normalize('NFKD', text)
+            .encode('ascii', 'ignore')
+            .decode('utf-8', 'ignore')
+            .lower())
+    words = re.sub(r'[^\w\s]', '', text).split()
+    #words = re.sub('http://\S+|https://\S+', '', text).split()
+    return [word for word in words if word not in stopwords]
 
 
+
+
+
+
+
+#used to select the columns to show in the dataframe for cleaning and making the bar charts
+columns = ['title', 'selftext' ]
+
+#funtion that cleans the unnerrcessary data from a coulmn in the dataframe eg. stopwords, links, etc.
+# makes trigram of list of words
+# @parametrs: df - the dataframe to clean
+# @return df - the cleaned dataframe
+def trigrams(df):
+    df = df[df[f'{columns[1]}'].str.contains("NaN") == False]
+    words = basic_clean(''.join(str(df[f'{columns[1]}'].tolist())))
+    trigrams_series = (pd.Series(nltk.ngrams(words, 3)).value_counts())[:10]
+    tg = pd.DataFrame({'trigrams': trigrams_series.index,
+                      'counts': trigrams_series.values})
+    return tg
+
+#funtion that cleans the unnerrcessary data from a coulmn in the dataframe eg. stopwords, links, etc.
+# makes bigram of list of words
+# @parametrs: df - the dataframe to clean
+# @return df - the cleaned dataframe
+def bigrams(df):
+    df = df[df[f'{columns[1]}'].str.contains("NaN") == False]
+    words = basic_clean(''.join(str(df[f'{columns[1]}'].tolist())))
+    bigrams_series = (pd.Series(nltk.ngrams(words, 2)).value_counts())[:10]
+    bg = pd.DataFrame({'bigrams': bigrams_series.index,
+                      'counts': bigrams_series.values})
+    return bg
+
+
+#funtion that makes the required bar charts bigrams
+#@parametrs: df - the dataframe to make the charts from
+def makeBarChartBigrams(df):
+    bg = bigrams(df)
+    bg.sort_values(by='counts', ascending=False, inplace=True)
+    bars = alt.Chart(bg,height=500).mark_bar().encode(
+        x='counts:Q',
+        y=alt.Y("bigrams:N", sort='-x')
+    )
+    st.altair_chart(bars, use_container_width=True)
+
+#funtion that makes the required bar charts bigrams
+#@parametrs: df - the dataframe to make the charts from
+def makeBarChartTrigrams(df):
+    tg = trigrams(df)
+    tg.sort_values(by='counts', ascending=False, inplace=True)
+    bars = alt.Chart(tg,height=500).mark_bar().encode(
+        x='counts:Q',
+        y=alt.Y("trigrams:N", sort='-x')
+    )
+    st.altair_chart(bars, use_container_width=True)
--- project2.py
+++ project2.py
@@ -5,22 +5,29 @@ import requests
 import streamlit as st
 import os
 from dotenv import load_dotenv
-from components import chart, displayDf, rating
+from components import chart, displayDf,makeBarChartBigrams,makeBarChartTrigrams
+import altair as alt
+from transformers import pipeline
 
 load_dotenv()
-
+st.set_page_config(layout='wide')
 st.title('Dashboard for Reddit')
-st.write('This is a simple app to get the hot posts from the subreddit of your choice')
 
+col1, col2, col3 = st.columns([0.4,2.5,0.4])
 subreddit = "wallstreetbets" 
-subreddit = st.text_input('Enter subreddit name', subreddit)
 
-type =st.selectbox('select type?', ('hot', 'new',))
-limit = st.selectbox(
-    'select Limit?',
-    ('10', '20', '30'))
+with st.container():
+    with col1:
+        st.markdown('#')
+        subreddit = st.text_input('Enter subreddit name', subreddit)
+        type =st.selectbox('select type?', ('hot', 'new',))
+        limit =  st.selectbox(
+            'select Limit?',
+            ('50','100'))
 
 
+
+#logic section
 secret_key = os.getenv('secret_key')
 client_id = os.getenv('client_id')
 auth = requests.auth.HTTPBasicAuth(client_id, secret_key)
@@ -41,33 +48,53 @@ header = {"Authorization": f"bearer {token}", "User-Agent": "'MyAPI/0.0.1'"}
 
 res_hot = requests.get(
     f'https://oauth.reddit.com/r/{subreddit}/{type}', headers=header, params={'limit': limit}) # get the hot/new posts from the subreddit
+#logic section end
+
+with st.container():
+    with col2:
+        st.write(f'{type} posts from {subreddit}')
+        dataFrameOfRedditPosts=displayDf(res_hot)
+        st.dataframe(dataFrameOfRedditPosts,use_container_width=True)
+
+mean=dataFrameOfRedditPosts['Sentiment_score'].mean()
+mean=round(mean,4)
+avg=dataFrameOfRedditPosts['Sentiment'].value_counts(normalize=True).mul(100).round(1).astype(str) + '%'
+
+avg.to_dict()
+
+with st.container():
+    with col3:
+        st.markdown('#')
+        st.write(f'Average Sentiment: ')
+        st.write("Positive",avg['POS'])
+        st.write("Negative",avg['NEG'])
+        st.write("Neutural",avg['NEU'])
+        st.write(f'mean Sentiment: {mean}')
 
 
+cc1,cc2,cc3=st.columns([0.4,2.5,0.4])
+with cc1:
+    st.markdown('#')
+    select='bitcoin'
+    select = st.text_input('Enter coin name', select)
+    interval = st.selectbox('Interval ?', (1,2,3))
+with cc2:
+    st.write(f'Chart of : {select}')
+    pr=chart(select, interval)
+with cc3:
+    st.markdown('#')
+    st.write(f'max price  is {round(pr[0])}')
+    st.write(f'min price  is {round(pr[1])}')
+    st.write(f'mean price  is {round(pr[2])}')
 
-df_hot_posts = pd.DataFrame()
-st.write(f'{type} posts from {subreddit}')
+c1,c2,c3,c4=st.columns([0.2,1,1,0.2])
 
-st.dataframe(displayDf(res_hot, df_hot_posts),use_container_width=True)
+with c2:
+    makeBarChartBigrams(dataFrameOfRedditPosts)
+with c3:
+    makeBarChartTrigrams(dataFrameOfRedditPosts)
 
-select='bitcoin'
-select = st.text_input('Enter coin name', select)
-interval = st.selectbox('Interval ?', (1,2,3))
 
-st.write(f'Chart of : {select}')
-st.button('Show Chart',on_click=chart(select, interval))
-
-textRate=pd.DataFrame()
-
-for post in res_hot.json()['data']['children']:
-    if(post['data']['selftext'] == ''): # if the post has no text, skip it
-        continue
-    textRate = textRate.append(
-        {
-            'Text': post['data']['selftext'],
-            'positivity Rating': rating(post['data']['selftext']),
-        }, ignore_index=True)
-
-st.dataframe(textRate)
 
 
 
